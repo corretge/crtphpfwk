0947dfc6a8c4a5753b9b1d2b2d13a56e2f6c3aed
Àlex Corretgé <alex@corretge.cat>
2008-08-26 21:35:10 +0000

    afegim SimpleForm

---
+++ crtml/crtml.sform.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * creació d'un formulari senzill
+ */
+
+/**
+ * recuperem les classes crtml
+ */
+$dir = dirname(__FILE__);
+require_once $dir . '/crtml.classes.php';
+
+class sform
+{
+	protected $id;
+	protected $form;
+	protected $fldset;
+	protected $butonera;
+	
+	public function __construct($titol, $id = 'UNIQUEID')
+	{
+		if ($id = 'UNIQUEID')
+		{
+			$this->id = uniqid('crtml');
+		}
+		else
+		{
+			$this->id = $id;
+		}
+		
+		$this->form = new crtmlFORM('');
+		$this->form->setId($id);
+		
+		
+		$this->fldset = new crtmlFIELDSET(new crtmlLEGEND($titol));
+		
+		$this->butonera = new crtmlDIV('butonera','','butonera');
+		
+	}
+	
+	public function addInput($label, $field, $type, $value)
+	{
+		$lb = new crtmlLABEL($label);
+		$fld = new crtmlINPUT($field);
+		$fld->setValue($value);
+		$fld->setType($type);
+		$lb->addContingut($fld);
+		$this->fldset->addContingut($lb);
+	}
+	
+	public function addButton($label, $action)
+	{
+		$btn = new crtmlINPUT($label);
+		$btn->setType('Submit');
+		$this->butonera->addContingut($btn);
+	}
+	
+	public function Render()
+	{
+		$this->form->addContingut($this->fldset);
+		$this->form->addContingut($this->butonera);
+		return $this->form->Render();
+	}
+}
+?>
